57a6436412c5827e1e794f3fbc4be6e4b8bc073f
Carlos Torres Gonzalez <e_ctor01@mercadona.es>
2021-05-18 15:36:04 +0200

    Messing around property binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,20 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
-  // styleUrls: ['./servers.component.css']
-  styles: [`
-      p {
-        color: green
-      }
-    `]
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent implements OnInit {
+export class ServersComponent  {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {this.allowNewServer = true}, 2000);
+  }
 
   ngOnInit(): void {
   }
