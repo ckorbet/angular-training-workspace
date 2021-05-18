d806ca44f569b4849235c24c91f3e036313715cf
Carlos Torres Gonzalez <e_ctor01@mercadona.es>
2021-05-18 15:23:54 +0200

    Playing with string interpolation

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -6,6 +6,10 @@ import { Component } from "@angular/core";
 })
 export class ServerComponent {
   serverId: number = 10;
-  serverStatus: string = 'offline';
+  serverStatus: string = 'Up & Running';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
 
 }
