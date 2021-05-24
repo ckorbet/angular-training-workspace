320a2b6074073664c77198b99d9436cd37629ee7
Carlos Torres Gonzalez <e_ctor01@mercadona.es>
2021-05-24 12:44:41 +0200

    First recipe model

--- my-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'This is just a test', 'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?output-format=auto&output-quality=auto')
+  ];
 
   constructor() { }
 
