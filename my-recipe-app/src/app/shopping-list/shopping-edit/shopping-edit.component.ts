5798432354720dc3218fd4bbe5719f0ea5c59be6
Carlos Torres Gonzalez <e_ctor01@mercadona.es>
2021-05-27 16:47:01 +0200

    Adding new ingredient with databindigs

--- my-recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ my-recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput', {static: false}) nameInputRed: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInputRed: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem() {
+    const ingName = this.nameInputRed.nativeElement.value;
+    const ingAmount = this.amountInputRed.nativeElement.value
+    this.ingredientAdded.emit(new Ingredient(ingName, ingAmount));
   }
 
 }
